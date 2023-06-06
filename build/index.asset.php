7e472c6810b9fc91201a98bd0cfdc0cf44551c3c
saiminh <saiminh@gmail.com>
2023-06-06 14:10:56 +0200

    mobile style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1705c9c627c27a323823');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '715c19d21e067f92d578');
